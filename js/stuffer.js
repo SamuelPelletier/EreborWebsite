9346e21aef7e0ce985a84daaddd4892b676145bf
Sam <samuel.pelletier@hotmail.fr>
2017-12-07 22:48:30 +0100

    Weather best

--- js/stuffer.js
+++ js/stuffer.js
@@ -2,7 +2,7 @@
  * Created by Samuel on 07/12/2017.
  */
 
-getMeteo('chambery');
+getMeteo('savoie');
 
 function push(val){
     $(".container").append("<div>BOB</div>");
@@ -10,20 +10,28 @@ function push(val){
 
 
 function getMeteo(city){
-    $.ajax({
-        url : 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22'+city+'%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys',
-        type : 'GET',
-        dataType : 'json',
-        success : function(data){
-            weatherToday = data.query.results.channel.item.forecast[0];
-            console.log(weatherToday)
-            $(".weather").html("<div>"+weatherToday.date+"<br/>"+Math.floor((weatherToday.low-32)*5/9)+"   "+Math.floor((weatherToday.high-32)*5/9)+"    "+weatherToday.text+"</div>").css("color","red");
-        },
+    var lon;
+    var lat;
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (data) {
+            lon = data.coords.latitude;
+            lat = data.coords.longitude;
+            $.ajax({
+                url: 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22(' + lon + '%2C' + lat + ')%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys',
+                type: 'GET',
+                dataType: 'json',
+                success: function (data) {
+                    weatherToday = data.query.results.channel.item.forecast[0];
+                    $(".weather").html("<div>"+data.query.results.channel.location.city+"<br/>" + weatherToday.date + "<br/>Min: " + Math.floor((weatherToday.low - 32) * 5 / 9) + "°C   Max: " + Math.floor((weatherToday.high - 32) * 5 / 9) + "°C<br/>" + weatherToday.text + "</div>").css("color", "red");
+                },
+            });
+        })
+    }
 
-        error : function(resultat, statut, erreur){
-        }
 
-    });
+}
 
-
+function showPosition(position) {
+    x.innerHTML = "Latitude: " + position.coords.latitude +
+        "<br>Longitude: " + position.coords.longitude;
 }
